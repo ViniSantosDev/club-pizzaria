fdf3f8471542b79ccf211fc7977bf56dc0e97610
Vinicius Santos <viniciusilvasantos.contact@gmail.com>
2023-05-15 18:58:24 -0300

    creating packages

---
+++ src/main/java/com/ViniSantosDev/clubpizzaria/ClubPizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.ViniSantosDev.clubpizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubPizzariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ViniSantosDev/clubpizzaria/ClubPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ViniSantosDev.clubpizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubPizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
