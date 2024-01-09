cfda6e601fdb0b03de90af9c62c7f78147b53113
Vinicius Santos <viniciusilvasantos.contact@gmail.com>
2024-01-09 17:19:08 -0300

    feat: adjust endpoint get by name in restaurants, using like with jpql

--- src/main/java/br/com/vinisantos/dev/clubpizzaria/repository/impl/CozinhaRepositoryImpl.java
+++ src/main/java/br/com/vinisantos/dev/clubpizzaria/repository/impl/CozinhaRepositoryImpl.java
@@ -22,9 +22,12 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 	public List<Cozinha> listar() {
 		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 	}
+
 	@Override
 	public List<Cozinha> consultarPorNome(String nome) {
-		return manager.createQuery("from Cozinha where nome =:nome", Cozinha.class).setParameter("nome", nome).getResultList();
+		return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+				.setParameter("nome", "%" + nome + "%")
+				.getResultList();
 	}
 
 	@Override
